4901e6ef5c42dffab200428b03b7c1edf2984ab5
jessicauk <jessica_220691@hotmail.com>
2017-01-31 13:52:43 -0600

    consume api usisng fetch method

--- app/components/block-component.jsx
+++ app/components/block-component.jsx
@@ -1,8 +1,32 @@
 const react = require('react');
 
-const ComponentBlock = () => 
-	<div>
-		<h1> Block component </h1>
-	</div>
+class ComponentBlock extends React.Component{
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: {},
+		}
+		
+	}
+	componentWillMount() {
+		const src = 'http://localhost:4000/getData'
+		fetch(src)
+			.then(response => response.json())
+			.then(responseSrc => {
+				this.setState.items = responseSrc.data;
+				console.log(responseSrc);
+			})
+	}
+
+	render(){
+		return (
+			<div>
+				<h1> Block component </h1>
+			</div>
+		)
+	}
+	
+}
 
 module.exports = ComponentBlock;
